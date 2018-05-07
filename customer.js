da6ce89bbe18e974362e4a6f4ceab63346adca48
Kendra Kyndberg <kendrak922@gmail.com>
2018-05-07 11:05:09 -0500

    got inquirer and mysql working

--- customer.js
+++ customer.js
@@ -1,5 +1,15 @@
 
 const inquirer = require('inquirer');
-inquirer.prompt([/* Pass your questions in here */]).then(answers => {
-    // Use user feedback for... whatever!!
+inquirer.prompt([
+    {   type: "input",
+        name: "idNumber",
+        message: "What is the ID of the product you would like to buy?"
+    },
+    {   type: "input",
+        name: "howMany",
+        message: "How many units of this product would you like to buy??"
+    }
+]).then(answers => {
+    
+        // Use user feedback for... whatever!!
 });
