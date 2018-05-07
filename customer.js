28857cbbaef6b7bce6ae2d495ba8d6a08b75d485
Kendra Kyndberg <kendrak922@gmail.com>
2018-05-07 13:35:49 -0500

    got database to subtract

--- customer.js
+++ customer.js
@@ -1,5 +1,34 @@
+"use strict";
 
+const mysql = require("mysql");
 const inquirer = require('inquirer');
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "shrimp93",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  displayProducts();
+});
+
+function displayProducts() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+  });
+}
+
+/// ID Number question
 inquirer.prompt([
     {   type: "input",
         name: "idNumber",
@@ -10,6 +39,22 @@ inquirer.prompt([
         message: "How many units of this product would you like to buy??"
     }
 ]).then(answers => {
+      
+        connection.query("SELECT * FROM products WHERE ?", {item_id : answers.idNumber}, function(err, res) {
+          if (err) throw err;
+          console.log(res);
+       connection.query("UPDATE products SET ? WHERE ?",
+          [
+            {
+              stock_quantity: (res[0].stock_quantity-answers.howMany)
+            },
+            {
+              item_id: answers.idNumber
+            }
+          ],
+      )
+      console.log("order is on the way!")
+      
     
-        // Use user feedback for... whatever!!
 });
+ });
